9eb62156459a6ab4bd6c84033f4b1440659ccb4b
abdulrahim0966 <abok0965@gmail.com>
2025-05-18 15:58:34 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // /js/config.js
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwflj2Xkz44KXcAydTq4iuVzX2IqrOv9dHcnEqI9_QHsF0nXf7v3Xa4Qt9BUh3hoqIC/exec";
+const GAS_IFRAME_URL = "https://script.google.com/macros/s/AKfycbwflj2Xkz44KXcAydTq4iuVzX2IqrOv9dHcnEqI9_QHsF0nXf7v3Xa4Qt9BUh3hoqIC/exec";

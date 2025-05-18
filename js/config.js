76e3cbdafd6eb9d9d70447ff91a3f538d9cb816f
abdulrahim0966 <abok0965@gmail.com>
2025-05-18 16:11:48 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,4 @@
-// /js/config.js
-const GAS_IFRAME_URL = "https://script.google.com/macros/s/AKfycbwflj2Xkz44KXcAydTq4iuVzX2IqrOv9dHcnEqI9_QHsF0nXf7v3Xa4Qt9BUh3hoqIC/exec";
+// js/config.js
+const CONFIG = {
+  IFRAME_URL: "https://n-xxxxxxx-script.googleusercontent.com", // iframe web app URL'si
+};

957d4463c3cf69c0ab30460ec987b354b3869c53
abdulrahim0966 <abok0965@gmail.com>
2025-05-18 14:28:31 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://script.google.com/macros/s/XXXXXXX/exec'; // kendi Google Apps Script URL'niz buraya gelecek
+export const API_URL = 'https://script.google.com/macros/s/AKfycbwflj2Xkz44KXcAydTq4iuVzX2IqrOv9dHcnEqI9_QHsF0nXf7v3Xa4Qt9BUh3hoqIC/exec'; // kendi Google Apps Script URL'niz buraya gelecek

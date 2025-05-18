b9d9f29f3af8e780bb729501dc250acbc002662e
abdulrahim0966 <abok0965@gmail.com>
2025-05-18 14:33:51 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
-export const API_URL = 'https://script.google.com/macros/s/AKfycbwflj2Xkz44KXcAydTq4iuVzX2IqrOv9dHcnEqI9_QHsF0nXf7v3Xa4Qt9BUh3hoqIC/exec'; // kendi Google Apps Script URL'niz buraya gelecek
+// /js/config.js
+const CONFIG = {
+  // Google Apps Script URL (Deploy edince çıkan "Web app URL" buraya gelecek)
+  GAS_URL: "https://script.google.com/macros/s/AKfycbwflj2Xkz44KXcAydTq4iuVzX2IqrOv9dHcnEqI9_QHsF0nXf7v3Xa4Qt9BUh3hoqIC/exec"
+};
